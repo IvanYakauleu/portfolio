32f556845839bb95bdf9fa4b4d9185a9a542ddff
ivan <voodoolar333@gmail.com>
2023-07-05 13:32:47 +0200

    add potfolio

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,8 @@
 const hamburger = document.querySelector('.hamburger'),
       closeMenu = document.querySelector('.promo__close'),
       promoMenu = document.querySelector('.promo__menu'),
-      overflow = document.querySelector('.overflow');
+      overflow = document.querySelector('.overflow'),
+      menuLinks = document.querySelectorAll('.promo__menu ul li a')
 
 hamburger.addEventListener('click', () => {
     promoMenu.classList.add('show');
@@ -13,8 +14,17 @@ hamburger.addEventListener('click', () => {
     document.body.style.overflow = 'hidden';
 });
 
-closeMenu.addEventListener('click', () => {
+function toCloseMenu() {
     overflow.classList.remove('show');
     promoMenu.classList.remove('show');
     document.body.style.overflow = '';
+}
+closeMenu.addEventListener('click', () => {
+    toCloseMenu()
+})
+
+menuLinks.forEach(item => {
+    item.addEventListener('click', () => {
+        toCloseMenu()
+    })
 })
