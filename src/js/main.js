41fa78cc67966a10e61eab7893e011fd357f2811
ivan <voodoolar333@gmail.com>
2023-07-05 16:41:38 +0200

    add contacts and price

--- src/js/main.js
+++ src/js/main.js
@@ -4,19 +4,22 @@
 
 const hamburger = document.querySelector('.hamburger'),
       closeMenu = document.querySelector('.promo__close'),
+      promoWrapper = document.querySelector('.promo__wrapper'),
       promoMenu = document.querySelector('.promo__menu'),
       overflow = document.querySelector('.overflow'),
-      menuLinks = document.querySelectorAll('.promo__menu ul li a')
+      menuLinks = document.querySelectorAll('.promo__menu ul li a');
 
 hamburger.addEventListener('click', () => {
     promoMenu.classList.add('show');
     overflow.classList.add('show');
+    promoWrapper.classList.add('activ');
     document.body.style.overflow = 'hidden';
 });
 
 function toCloseMenu() {
     overflow.classList.remove('show');
     promoMenu.classList.remove('show');
+    promoWrapper.classList.remove('activ');
     document.body.style.overflow = '';
 }
 closeMenu.addEventListener('click', () => {
